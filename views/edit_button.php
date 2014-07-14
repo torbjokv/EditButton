56e8b9076da8b4681ceb2e8a44c3e5519f4466c9
Torbjørn Kvåle <torbjoernkvaale@gmail.com>
2014-07-14 13:35:51 +0200

    Fixed paths

--- views/edit_button.php
+++ views/edit_button.php
@@ -1,4 +1,4 @@
-<link href="/admin/modules/addons/EditButton/assets/css/edit_button.css?v=1.0" type="text/css" rel="stylesheet" />
-<a class="resetclass EditButton_button" href="<?php echo $cockpitFolder; ?>/index.php/regions/region/<?php echo $regionId; ?>">
+<link href="/<?php echo $cockpitFolder; ?>/modules/addons/EditButton/assets/css/edit_button.css?v=1.0" type="text/css" rel="stylesheet" />
+<a class="resetclass EditButton_button" href="/<?php echo $cockpitFolder; ?>/index.php/regions/region/<?php echo $regionId; ?>">
 Edit
 </a>
