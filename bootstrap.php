0fb163389ce319ed7685cb752289008836bb345b
Torbjørn Kvåle <torbjoernkvaale@gmail.com>
2014-07-14 11:50:14 +0200

    initial commit

---
+++ bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+
+if($app->module("auth")->getUser() && $app->module("auth")->hasaccess("Regions", ['create.regions', 'edit.regions'])) {
+
+    $regionRenderer = $this->module("regions")->__get('render');
+
+    $this->module("regions")->extend([
+        "render" => function($name, $params = []) use($regionRenderer, $app) {
+            $region = $app->db->findOne("common/regions", ["name"=>$name]);
+            $output = $regionRenderer($name, $params);
+            $path = dirname(__FILE__);
+            $cockpitFolder = explode('/',$path)[sizeof(explode('/',$path))-4];
+            $regionId = $region['_id'];
+            $output .= $app->renderer()->file(
+                $path."/views/edit_button.php", 
+                compact(
+                    'cockpitFolder', 
+                    'regionId'
+                    )
+                );
+            
+            return $output;
+        }
+    ]);
+}
